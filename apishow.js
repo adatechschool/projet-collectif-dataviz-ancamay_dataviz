0a362f20e49701906818f300b54f2dcc96555c52
AlbaMate <alba.dutel@gmail.com>
2024-03-25 11:22:35 +0100

    add hello from mayte

--- apishow.js
+++ apishow.js
@@ -1,19 +1,20 @@
-var ContenuName = document.getElementById('drago');
+var ContenuName = document.getElementById("drago");
 
-var endPoint = 'https://api.tvmaze.com/shows'
-
+var endPoint = "https://api.tvmaze.com/shows";
 
 for (var i = 1; i < 10; i++) {
-    ajaxGet(endPoint + i, function (response) {
-        var contenu;
-        var information = JSON.parse(response);
-        contenu = '<ul>';
-        contenu += '<li>' + information.name + '</li>';
-        contenu += '<li>' + information.types[0].type.name + '</li>';
-        ContenuName.innerHTML += contenu;
-        console.log(contenu);
-    });
+  ajaxGet(endPoint + i, function (response) {
+    var contenu;
+    var information = JSON.parse(response);
+    contenu = "<ul>";
+    contenu += "<li>" + information.name + "</li>";
+    contenu += "<li>" + information.types[0].type.name + "</li>";
+    ContenuName.innerHTML += contenu;
+    console.log(contenu);
+  });
 }
-{/* <li>
+{
+  /* <li>
                             <p id="drago" class="mr-16 text-xl">Les informations arrivent..</p>
-                        </li> */}
+                        </li> */
+}
